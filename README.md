3de04a52788853a55ac2fe9311b8d7cdf8372359
linyiLYi <48440925+linyiLYi@users.noreply.github.com>
2023-04-06 23:35:08 +0800

    Update README and model files

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # SFighterAI 街头霸王游戏智能代理
 
-本项目基于深度强化学习训练了一个用于通关《街头霸王·二：冠军特别版》（Street Fighter II Special Champion Edition）关地 BOSS 的智能 AI 代理。该智能代理完全基于游戏画面（RGB 像素值）进行决策，在该项目给定存档中最后一关的第一轮对局可以取得 100% 胜率（实际上出现了“过拟合”现象，详见[结果]部分的讨论）。
+本项目基于深度强化学习训练了一个用于通关《街头霸王·二：冠军特别版》（Street Fighter II Special Champion Edition）关底 BOSS 的智能 AI 代理。该智能代理完全基于游戏画面（RGB 像素值）进行决策，在该项目给定存档中最后一关的第一轮对局可以取得 100% 胜率（实际上出现了“过拟合”现象，详见[结果]部分的讨论）。
 
 ### 文件结构
 
 ```bash
+├───data
 ├───main
 │   ├───logs
 │   ├───trained_models
@@ -13,21 +14,46 @@
 │   └───scripts
 ```
 
-项目的主要文件夹为 `main/`。其中，`logs/` 中包含了记录训练过程的终端文本和数据曲线（使用 Tensorboard 查看）；`trained_models/` 中包含了不同阶段的模型权重文件，可以用于在 `test.py` 中运行测试，观看智能代理在不同训练阶段学习到的对战策略的效果。
+游戏配置文件存储在 `data/` 文件夹下；项目的主要代码文件夹为 `main/`。其中，`logs/` 中包含了记录训练过程的终端文本和数据曲线（使用 Tensorboard 查看）；`trained_models/` 中包含了不同阶段的模型权重文件，可以用于在 `test.py` 中运行测试，观看智能代理在不同训练阶段学习到的对战策略的效果。
 
 ## 运行指南
 
-本项目基于 Python 编程语言，主要使用了 [OpenAI Gym Retro](https://retro.readthedocs.io/en/latest/getting_started.html)、[Stable-Baselines3](https://stable-baselines3.readthedocs.io/en/master/) 等标准代码库。程序运行使用的 Python 版本为 3.8.10，建议使用 [Anaconda](https://www.anaconda.com) 配置 Python 环境。
+本项目基于 Python 编程语言，主要使用了 [OpenAI Gym Retro](https://retro.readthedocs.io/en/latest/getting_started.html)、[Stable-Baselines3](https://stable-baselines3.readthedocs.io/en/master/) 等标准代码库。程序运行使用的 Python 版本为 3.8.10，建议使用 [Anaconda](https://www.anaconda.com) 配置 Python 环境。以下配置过程已在 Windows 11 系统上测试通过。以下为控制台/终端（Console/Terminal/Shell）指令：
+
+### 环境配置
 
 ```bash
 # 创建 conda 环境，将其命名为 StreetFighterAI，Python 版本 3.8.10
 conda create -n StreetFighterAI python=3.8.10
 conda activate StreetFighterAI
 
-# 注：conda 在苹果 M1 芯片（Apple Silicon）上对 python 向下支持到 3.8.11，使用以下指令创建环境：
-# conda create -n StreetFighterAI python=3.8.11
-
 # 安装 Python 代码库
-cd [project_dir]/street-fighter-ai/main
+cd [项目上级文件夹]/street-fighter-ai/main
 pip install -r requirements.txt
+
+# 运行程序脚本定位 gym-retro 游戏文件夹位置
+cd ..
+python .\utils\print_game_lib_folder.py
 ```
+
+控制台输出文件夹路径后，将其复制到文件资源管理器中，跳转到对应路径。该文件夹为 gym-retro 下《街头霸王·二：冠军特别版》的游戏数据文件夹，其中包含了游戏 ROM 文件和数据配置文件。将本项目中 `data/` 文件夹下的 `Champion.Level12.RyuVsBison.state`、`data.json`、`metadata.json`、`scenario.json` 四个文件复制到该文件夹中，覆盖原有文件（可能需要提供管理员权限）。其中 `.state` 文件为《街头霸王·二：冠军特别版》难度四最后一关开局的游戏存档，三个 `.json` 文件为 gym-retro 配置文件，存储了游戏信息的内存地址（本项目只用到了其中的 [agent_hp] 与 [enemey_hp]，用于实时读取游戏人物的生命值）。
+
+运行程序还需要《街头霸王·二：冠军特别版》（Street Fighter II Special Champion Edition）的游戏 ROM 文件（可以理解为游戏程序本身）。gym-retro 本身不提供游戏的 ROM 文件，需要自行通过合法途径获得。可以参考该[链接](https://wowroms.com/en/roms/sega-genesis-megadrive/street-fighter-ii-special-champion-edition-europe/26496.html)。
+
+通过合法途径自行获得游戏 ROM 文件后，将其复制到前述 gym-retro 的游戏数据文件夹下，并重命名为`rom.md`。至此，环境配置准备工作完成。
+
+注：如果想要录制智能代理的对战视频，还需要安装 [ffmpeg](https://ffmpeg.org/)。
+```bash
+conda install ffmpeg
+```
+
+### 运行测试
+
+环境配置完成后，可以在 `main/` 文件夹下运行 `test.py` 进行测试，实际体验智能代理在不同训练阶段的表现。
+
+```bash
+cd [项目上级文件夹]/street-fighter-ai/main
+python test.py
+```
+
+模型权重文件存储在 `main/trained_models/` 文件夹下。
--- main/test.py
+++ main/test.py
@@ -6,15 +6,20 @@ from stable_baselines3 import PPO
 
 from street_fighter_custom_wrapper import StreetFighterCustomWrapper
 
-RESET_ROUND = False # Reset the round when fight is over. 
-RENDERING = True
-RECORDING = True
+RESET_ROUND = True # Whether to reset the round when fight is over. 
+RENDERING = True    # Whether to render the game screen.
+
+MODEL_NAME = r"ppo_ryu_2500000_steps_updated" # Speicify the model file to load. Model "ppo_ryu_2500000_steps_updated" is capable of beating the final stage (Bison) of the game.
+
+# Model notes:
+# ppo_ryu_2000000_steps_updated: Just beginning to overfit state, generalizable but not quite capable.
+# ppo_ryu_2500000_steps_updated: Approaching the final overfitted state, cannot dominate first round but partially generalizable. High chance of beating the final stage.
+# ppo_ryu_3000000_steps_updated: Near the final overfitted state, almost dominate first round but barely generalizable.
+# ppo_ryu_7000000_steps_updated: Overfitted, dominates first round but not generalizable. 
 
 RANDOM_ACTION = False
+NUM_EPISODES = 30 # Make sure NUM_EPISODES >= 3 if you set RESET_ROUND to False to see the whole final stage game.
 MODEL_DIR = r"trained_models/"
-MOVIE_DIR = r"recordings"
-
-MODEL_NAME = r"ppo_ryu_7000000_steps"
 
 def make_env(game, state):
     def _init():
@@ -35,24 +40,23 @@ env = make_env(game, state="Champion.Level12.RyuVsBison")()
 if not RANDOM_ACTION:
     model = PPO.load(os.path.join(MODEL_DIR, MODEL_NAME), env=env)
 
-# obs = env.reset()
+obs = env.reset()
 done = False
 
-num_episodes = 30
+num_episodes = NUM_EPISODES
 episode_reward_sum = 0
 num_victory = 0
+
+print("\nFighting Begins!\n")
+
 for _ in range(num_episodes):
     done = False
-    obs = env.reset()
-
-    if RECORDING:
-        # Start recording
-        movie_path = os.path.join(MOVIE_DIR, "{}.bk2".format(MODEL_NAME))
-        env.unwrapped.movie = retro.Movie(movie_path, retro.MovieMode.RECORD)
-        env.unwrapped.movie.step()
+    
+    if RESET_ROUND:
+        obs = env.reset()
 
     total_reward = 0
-    
+
     while not done:
         timestamp = time.time()
 
@@ -61,25 +65,26 @@ for _ in range(num_episodes):
         else:
             action, _states = model.predict(obs)
             obs, reward, done, info = env.step(action)
-        
-        if RECORDING:
-            # Record the step
-            env.unwrapped.movie.step()
 
         if reward != 0:
             total_reward += reward
             print("Reward: {:.3f}, playerHP: {}, enemyHP:{}".format(reward, info['agent_hp'], info['enemy_hp']))
-    
-    if RECORDING:
-        # Stop recording
-        env.unwrapped.movie.close()
-        del env.unwrapped.movie
+        
+        if info['enemy_hp'] < 0 or info['agent_hp'] < 0:
+            done = True
 
     if info['enemy_hp'] < 0:
         print("Victory!")
         num_victory += 1
-    print("Total reward: {}".format(total_reward))
+
+    print("Total reward: {}\n".format(total_reward))
     episode_reward_sum += total_reward
+
+    if not RESET_ROUND:
+        while info['enemy_hp'] < 0 or info['agent_hp'] < 0:
+        # Inter scene transition. Do nothing.
+            obs, reward, done, info = env.step([0] * 12)
+            env.render()
 
 env.close()
 print("Winning rate: {}".format(1.0 * num_victory / num_episodes))
